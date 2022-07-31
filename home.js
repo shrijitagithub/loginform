48c8bf1ab060afcdf22ec6b32175983158e35073
shrijitagithub <108064688+shrijitagithub@users.noreply.github.com>
2022-07-31 04:55:00 -0700

    Add files via upload

---
+++ home.js
@@ -0,0 +1,15 @@
+const greeting = document.querySelector('.greeting');
+
+window.onload = () => {
+    if(!sessionStorage.name){
+        location.href = '/login';
+    }else{
+        greeting.innerHTML = 'hello ${sessionStorage.name}';
+    }
+}
+
+const logout = document.querySelector('.logout');
+logout.onclick = () => {
+    sessionStorage.clear();
+    location.reload();
+}
